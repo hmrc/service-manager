361a5ef8b777b5e19460f5232f1dacb6d174b744
Vaughan Sharman <vaughansharman@hotmail.com>
2014-07-24 20:33:18 +0100

    PIPPIFY Adding smserver to be an entry point

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 #    required = f.read().splitlines()
 
 setup(name='servicemanager',
-      version='0.0.4',
+      version='0.0.5',
       description='A python tool to manage developing and testing with lots of microservices',
       url='https://github.com/hmrc/service-manager',
       author='vaughansharman',
       license='Apache Licence 2.0',
       packages=['servicemanager', 'servicemanager.actions', 'servicemanager.server', 'servicemanager.service', 'servicemanager.thirdparty'],
       install_requires=['requests==2.2.1','pymongo==2.6.3','bottle==0.12.4','pytest==2.5.2'],
-      scripts=['bin/sm'],
+      scripts=['bin/sm', 'bin/smserver'],
       zip_safe=False)
