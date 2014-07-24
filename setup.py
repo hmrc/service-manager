c6a9d6b16dc280831b72d8c9fecb944f834b57bf
Vaughan Sharman <vaughansharman@hotmail.com>
2014-07-24 17:33:28 +0100

    PIPPIFY now pushed to pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+#with open('requirements.txt') as f:
+#    required = f.read().splitlines()
 
 setup(name='servicemanager',
-      version='0.0.1',
+      version='0.0.4',
+      description='A python tool to manage developing and testing with lots of microservices',
       url='https://github.com/hmrc/service-manager',
       author='vaughansharman',
-      licence='Apache Licence 2.0',
+      license='Apache Licence 2.0',
       packages=['servicemanager', 'servicemanager.actions', 'servicemanager.server', 'servicemanager.service', 'servicemanager.thirdparty'],
-      install_requires=required,
-      scripts=['bin/sm.py'],
+      install_requires=['requests==2.2.1','pymongo==2.6.3','bottle==0.12.4','pytest==2.5.2'],
+      scripts=['bin/sm'],
       zip_safe=False)
