edced9a96717f109d2179922291433ef580faeb4
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2015-08-25 14:45:15 +0100

    Bump version to 0.0.29

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='servicemanager',
-      version='0.0.28',
+      version='0.0.29',
       description='A python tool to manage developing and testing with lots of microservices',
       url='https://github.com/hmrc/service-manager',
       author='vaughansharman',
