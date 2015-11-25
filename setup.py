196baa9c2824c5d0409282f3b5cbe7c2f2ef01d5
Charles Kubicek <charles.kubicek@digital.hmrc.gov.uk>
2015-11-25 11:16:10 +0000

    bump version number and update author

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup
 
 setup(name='servicemanager',
-      version='0.1.0',
+      version='0.2.0',
       description='A python tool to manage developing and testing with lots of microservices',
       url='https://github.com/hmrc/service-manager',
-      author='vaughansharman',
+      author='hmrc-web-operations',
       license='Apache Licence 2.0',
       packages=['servicemanager', 'servicemanager.actions', 'servicemanager.server', 'servicemanager.service', 'servicemanager.thirdparty'],
       install_requires=['requests==2.2.1','pymongo==3.0.1','bottle==0.12.4','pytest==2.5.2','argcomplete==0.8.1'],
