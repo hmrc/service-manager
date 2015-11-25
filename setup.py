c80191c2dac36bd94c4c347fb5422f89ab42cc2e
Steve Smith <steve@alwaysagileconsulting.com>
2015-11-25 10:52:48 +0000

    Increment to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='servicemanager',
-      version='0.0.31',
+      version='0.1.0',
       description='A python tool to manage developing and testing with lots of microservices',
       url='https://github.com/hmrc/service-manager',
       author='vaughansharman',
