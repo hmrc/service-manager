cea07984dcd846b9694143f68d0380dea4e1ae8e
grahamhargreaves <grahamhargreaves@digital.cabinet-office.gov.uk>
2015-11-25 12:15:24 +0000

    Add env vars to fix the tests

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-py.test -v --junitxml results.xml test/tests.py 
+export JAVA_HOME=${JAVA_HOME:=$(/usr/libexec/java_home)}
+export WORKSPACE=${WORKSPACE:='.'}
+export NEXUS_PASS=${NEXUS_PASS:='fake'}
+export NEXUS_USER=${NEXUS_USER:='fake'}
+py.test -v --junitxml results.xml test/tests.py
 py.test -v --junitxml results.xml test/unit
