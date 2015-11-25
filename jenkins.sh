f675a43e2970784f60d0b92af47a62734d225b8e
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2015-11-25 15:37:24 +0000

    Update fake NEXUS_* creds in env vars to what tests expect

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export JAVA_HOME=${JAVA_HOME:=$(/usr/libexec/java_home)}
 export WORKSPACE=${WORKSPACE:='.'}
-export NEXUS_PASS=${NEXUS_PASS:='fake'}
-export NEXUS_USER=${NEXUS_USER:='fake'}
+export NEXUS_PASS=${NEXUS_PASS:='sbt_creds'}
+export NEXUS_USER=${NEXUS_USER:='sbt'}
 py.test -v --junitxml results.xml test/tests.py
 py.test -v --junitxml results.xml test/unit
