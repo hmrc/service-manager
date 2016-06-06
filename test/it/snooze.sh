d955af5d06d40129ac1eef3132f47a5e93dbb193
Oliver Wickham <mr.o.wickham@gmail.com>
2016-06-06 21:11:52 +0100

    Fix typo

--- test/it/snooze.sh
+++ test/it/snooze.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # this script is here to allow the testting of additional parameters being passed to a script.
-# the parameters are multiplied together. If they don't exist, they are defaulted to zeo
+# the parameters are multiplied together. If they don't exist, they are defaulted to zero
 sleep $(($1 * ${2:-1}))
