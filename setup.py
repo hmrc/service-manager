7fd927d505dc5edcdfe744665c5d05b3b3424a80
Jakob Grunig <jakob.grunig@digital.hmrc.gov.uk>
2018-07-16 15:07:52 +0100

    Bump release version to 1.4.0

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 
 from setuptools import setup
-import os
 
 setup(name='servicemanager',
       python_requires='>2.7.13',
-      version=os.environ.get("RELEASE_VERSION", "local-development-build"),
+      version='1.4.0',
       description='A python tool to manage developing and testing with lots of microservices',
       url='https://github.com/hmrc/service-manager',
       author='hmrc-web-operations',
