f5339fef22e1db5974a9de2583b6e153aecfa822
Christopher Turner <23338096+christopherjturner@users.noreply.github.com>
2020-08-10 13:47:08 +0100

    BDOG-914 rework smdownload, travis matrix

--- servicemanager/smdownload.py
+++ servicemanager/smdownload.py
@@ -3,33 +3,31 @@ import time
 import sys
 
 
-def _report_hook(count, block_size, total_size):
-    global start_time
-    global last_update
+def _current_milli_time():
+    return int(time.time() * 1000)
 
-    current_milli_time = lambda: int(time.time() * 1000)
 
-    if count == 0:
-        start_time = time.time()
-        last_update = current_milli_time()
-        return
+def _report_hook(count, block_size, total_size, start_time, last_update):
     duration = time.time() - start_time
-    progress_size = int(count * block_size)
+    progress_size = _current_milli_time()
     try:
         speed = int(progress_size / (1024 * duration))
     except ZeroDivisionError:
         speed = 0
 
     percent = int(count * block_size * 100 / total_size)
-    if percent == 100 or (current_milli_time() - last_update) > 500:
+    if percent == 100 or (_current_milli_time() - last_update) > 500:
         sys.stdout.write(
             "\r%d%%, %d MB, %d KB/s, %d seconds passed" % (percent, progress_size / (1024 * 1024), speed, duration)
         )
         sys.stdout.flush()
-        last_update = current_milli_time()
+        last_update = _current_milli_time()
+    return start_time, last_update
 
 
 def download(url, targetfile, show_progress=False, credentials=None):
+    start_time = time.time()
+    last_update = _current_milli_time()
     with requests.get(url, stream=True, auth=credentials) as r:
         with open(targetfile, "wb") as f:
             total_size = int(r.headers["Content-length"])
@@ -39,7 +37,7 @@ def download(url, targetfile, show_progress=False, credentials=None):
                 for chunk in r.raw.stream(chunk_size, decode_content=False):
                     f.write(chunk)
                     if show_progress:
-                        _report_hook(progress, chunk_size, total_size)
+                        start_time, last_update = _report_hook(progress, chunk_size, total_size, start_time, last_update)
                         progress = progress + 1
     if show_progress:
         print("\n")
