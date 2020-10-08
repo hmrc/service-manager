96b9d8810c8fe31c234410d47788a1c081b285a3
Christopher Turner <23338096+christopherjturner@users.noreply.github.com>
2020-10-08 08:38:04 +0100

    BDOG-914 bump version to 2.0.6 raise exception on -f

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="servicemanager",
-    version="2.0.5",
+    version="2.0.6",
     description="A python tool to manage developing and testing with lots of microservices",
     url="https://github.com/hmrc/service-manager",
     author="hmrc-web-operations",
